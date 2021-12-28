ced1c2ae880a5c2d2abdd550a26c97853cf3507d
ali 2236 <ali.gh2236@gmail.com>
2021-12-28 12:06:02 +0330

    singleton

--- src/main/kotlin/KotlinSingleton.kt
+++ src/main/kotlin/KotlinSingleton.kt
@@ -4,3 +4,30 @@ object KotlinSingleton {
     var myProperty : String? = null
     
 }
+
+/*
+final public class JavaSingleton {
+
+    private static JavaSingleton _instance;
+
+    public static JavaSingleton getInstance(){
+        if(_instance == null){
+            _instance = new JavaSingleton();
+        }
+        return  _instance;
+    }
+
+    private JavaSingleton(){}
+
+    private String myProperty;
+
+    public String getMyProperty() {
+        return myProperty;
+    }
+
+    public void setMyProperty(String myProperty) {
+        this.myProperty = myProperty;
+    }
+}
+
+ */
